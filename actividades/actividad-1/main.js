b35a487b7af6f01eddfdca2c0b10741cc99795c2
Cex <cesarpd@gmail.com>
2020-04-02 12:30:38 +0200

    actividad 1 - ejercico 3 Saludo

---
+++ actividades/actividad-1/main.js
@@ -0,0 +1,11 @@
+let campoTexto = document.getElementById('texto')
+let campoParra = document.getElementById('parrafo')
+let eboton = document.getElementById('boton')
+
+
+eboton.addEventListener('click', function mostrarNombre(e) {
+  if (campoTexto.value === "" || campoTexto.value.lenght == 0) {
+    alert("El campo esta vacio")
+  } else {
+    campoParra.innerHTML += `El testo de arriba es ${campoTexto.value}`;  }; 
+})
