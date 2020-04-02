f83ae3004f326938765597d4b7472c6275e8db33
Cex <cesarpd@gmail.com>
2020-04-02 17:38:58 +0200

    (prueba sin exito) insertar cada objeto en un ul separado

---
+++ actividades/actividad-2/prueba/main.js
@@ -0,0 +1,51 @@
+let peliculas = {
+  "listado": [{
+    "id": 1,
+    "nombre": "El sexto sentido",
+    "director": "M. Night Shyamalan",
+    "clasificacion": "Drama"
+  },
+  {
+    "id": 2,
+    "nombre": "Pulp Fiction",
+    "director": "Tarantino",
+    "clasificacion": "Acción"
+  },
+  {
+    "id": 3,
+    "nombre": "Todo Sobre Mi Madre",
+    "director": "Almodobar",
+    "clasificacion": "Drama"
+  }
+  ]
+};
+
+let eList = document.getElementById('lista');
+
+for (let i = 0; i < peliculas.listado.length; i++) {
+  let listado = peliculas.listado[i];
+  // Creamos un ul por cada objeto contenido en listado
+  let ulPelis = document.createElement('ul');
+  eList.appendChild(ulPelis);
+  // Añadimos las propiedades del objeto en una lista
+  console.log(Object.keys(peliculas.listado[i]));
+  
+  for (let n = 0; n < Object.keys(peliculas.listado[i]).length; n++) {
+    console.log(Object.keys(peliculas.listado[i]));
+    let liPeliId = document.createElement('li');
+    let liPeliNombre = document.createElement('li');
+    let liPeliDirector = document.createElement('li');
+    let liPeliClasificacion = document.createElement('li');
+    
+    liPeliId.textContent = listado.id;
+    liPeliNombre.textContent = listado.nombre;
+    liPeliDirector.textContent = listado.director;
+    liPeliClasificacion.textContent = lista;
+    
+    ulPelis.appendChild(liPeliId);
+    ulPelis.appendChild(liPeliNombre);
+    ulPelis.appendChild(liPeliDirector);
+    ulPelis.appendChild(liPeliClasificacion);
+    
+  }
+}
