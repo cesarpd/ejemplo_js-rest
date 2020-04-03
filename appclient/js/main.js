d99b79b665841901fdf2eda4f00cc97ccef66b3a
Cex <cesarpd@gmail.com>
2020-04-03 12:18:56 +0200

    Limpieza de codigo

--- appclient/js/main.js
+++ appclient/js/main.js
@@ -48,30 +48,5 @@ function init() {
   xhr.open('GET', url, true);
   xhr.send();
 
-// seleccionar la lista por id
-// let eLoad = document.querySelector('#alist p');
-// eLoad.style.display='none';
-
-// for (let i = 0; i < alumnos.length; i++) {
-//   const a = alumnos[i];
-//   eList.innerHTML += `
-//    <div class="col-lg-4 col-sm-6 mb-5 px-5">
-//           <div class="row d-flex align-items-center">
-//             <div class="col-5 avatar w-100 white d-flex justify-content-center align-items-center">
-//               <img src="${a.avatar}"
-//                 class="img-fluid rounded-circle z-depth-1" />
-//             </div>
-//             <div class="col-7">
-//               <h6 class="font-weight-bold pt-2">${a.nombre}</h6>
-//               <p class="text-muted">
-//                 Sexo: ${a.sexo}
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-//     `;
-
-  
-// }
 
 }
