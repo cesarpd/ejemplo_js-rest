e36bf258b0fe6c2fe0920530717fe64cc5c1ea2c
Cex <cesarpd@gmail.com>
2020-04-03 09:01:04 +0200

    listado alumnos desde Json version simple

--- appclient/js/main.js
+++ appclient/js/main.js
@@ -1,30 +1,71 @@
-// console.info('Para ver en el depurador del navegador');
-// console.debug('Debug');
-// console.trace('trace');
-// console.warn('warn');
-// console.error('error');
+window.addEventListener('load', init())
 
+function init() {
+  console.debug('Documennt loaded...')
+  const alumnos = [
+    {
+      "id": 1,
+      "nombre": "Oconor",
+      "avatar": "https://i.pravatar.cc/200?u=iptk0001",
+      "sexo": "Hombre"
+    },
+    {
+      "id": 2,
+      "nombre": "Pepa",
+      "avatar": "https://i.pravatar.cc/200?u=iptk0002",
+      "sexo": "Mujer"
+    },
+    {
+      "id": 3,
+      "nombre": "Jose Mari",
+      "avatar": "https://i.pravatar.cc/200?u=iptk0003",
+      "sexo": "Otro"
+    },    
+    {
+      "id": 4,
+      "nombre": "Luis",
+      "avatar": "https://i.pravatar.cc/200?u=iptk0004",
+      "sexo": "Mujer"
+    },
+    {
+      "id": 5,
+      "nombre": "Ramona",
+      "avatar": "https://i.pravatar.cc/200?u=iptk0005",
+      "sexo": "Mujer"
+    },
+    {
+      "id": 6,
+      "nombre": "Maritere",
+      "avatar": "https://i.pravatar.cc/200?u=iptk0006",
+      "sexo": "Hombre"
+    }    
+]
+
+// seleccionar la lista por id
 let eList = document.getElementById('alist');
 let eLoad = document.querySelector('#alist p');
-for (let i = 1; i < 10; i++) {
-  eLoad.style.display='none';
+eLoad.style.display='none';
+
+for (let i = 0; i < alumnos.length; i++) {
+  const a = alumnos[i];
   eList.innerHTML += `
    <div class="col-lg-4 col-sm-6 mb-5 px-5">
           <div class="row d-flex align-items-center">
             <div class="col-5 avatar w-100 white d-flex justify-content-center align-items-center">
-              <img src="https://i.pravatar.cc/200?u=iptk000${i}"
+              <img src="${a.avatar}"
                 class="img-fluid rounded-circle z-depth-1" />
             </div>
             <div class="col-7">
-              <h6 class="font-weight-bold pt-2">Name ${i}</h6>
+              <h6 class="font-weight-bold pt-2">${a.nombre}</h6>
               <p class="text-muted">
-                Description ${i} 
+                Sexo: ${a.sexo}
               </p>
             </div>
           </div>
         </div>
-  
-  `;
+    `;
 
   
+}
+
 }
